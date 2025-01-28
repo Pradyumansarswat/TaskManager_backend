a305d3a617d9189d28b6099e8d8f15dee4c559bb
Pradyuman Sarswat <pradyumansarswat@gmail.com>
2025-01-28 20:41:05 +0530

    index.js

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+
+app.get('/', (req, res) => {
+  res.send('Backend server is running');
+});
+
 app.use(
   cors({
     origin: ["http://localhost:3000", "http://localhost:3001"],
